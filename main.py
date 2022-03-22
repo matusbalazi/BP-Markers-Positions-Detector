c444938d028a577904060a732205bfa2e161ba4e
Matúš Baláži <balazi@stud.uniza.sk>
2022-03-22 18:24:01 +0100

    Testing of circle detector

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import perspective_transformation
 import circle_detector_with_cv
-import circle_detector_without_cv
 import argparse
 import sys
 import cv2
@@ -34,9 +33,27 @@ def main():
         coords = str(input("Zadajte suradnice rohov objektu urceneho na transformovanie (napr. [(327,303), (1128, 307), ...]): "))
 
     points = np.array(eval(coords), dtype="float32")
+    newImage = "after_perspective_transformation.jpg"
 
     perspective = perspective_transformation.PerspectiveTransformation(image, width, height, points)
-    cv2.imshow("Image", perspective.doTransformation())
+    transformedImage = perspective.doTransformation()
+    cv2.imshow("Image", transformedImage)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+    status = cv2.imwrite(newImage, transformedImage)
+
+    cv2.waitKey(1000000)
+
+    print("Obrazok bol ulozeny na disk: ", status)
+
+
+    minRadius = str(input("Zadajte najmensi polomer hladaneho kruhu: "))
+    maxRadius = str(input("Zadajte najvacsi polomer hladaneho kruhu: "))
+    objSize = str(input("Zadajte priemer kruhu v skutocnosti: "))
+
+    circleDetector = circle_detector_with_cv.CircleDetectorWithCV(newImage, int(minRadius), int(maxRadius), int(objSize))
+
+    cv2.imshow("Image", circleDetector.findAllCircles(1))
     cv2.waitKey(0)
 
 if __name__ == "__main__":
